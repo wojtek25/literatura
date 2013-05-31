236fd60f35f834de5d6a3363a6d4b0cd806d4a72
Piotr <kosz.piotr@gmail.com>
2013-05-31 10:06:38 +0200

    Author

---
+++ src/Gajdaw/LitBundle/Tests/Controller/AuthorControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Gajdaw\LitBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AuthorControllerTest extends WebTestCase
+{
+    /*
+    public function testCompleteScenario()
+    {
+        // Create a new client to browse the application
+        $client = static::createClient();
+
+        // Go to the list view
+        $crawler = $client->request('GET', '/author/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /author/");
+
+        // Go to the show view
+        $crawler = $client->click($crawler->selectLink('show')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code");
+    }
+
+    */
+}
