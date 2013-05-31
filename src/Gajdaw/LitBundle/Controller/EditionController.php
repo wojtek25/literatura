70a159a35d94ae93a0a56257de2b78e1b984f0e8
wojtek25 <wojciech.chec@gmail.com>
2013-05-31 09:48:27 +0200

    edition

---
+++ src/Gajdaw/LitBundle/Controller/EditionController.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace Gajdaw\LitBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Gajdaw\LitBundle\Entity\Edition;
+use Gajdaw\LitBundle\Form\EditionType;
+
+/**
+ * Edition controller.
+ *
+ * @Route("/edition")
+ */
+class EditionController extends Controller
+{
+    /**
+     * Lists all Edition entities.
+     *
+     * @Route("/", name="edition")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('GajdawLitBundle:Edition')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Creates a new Edition entity.
+     *
+     * @Route("/", name="edition_create")
+     * @Method("POST")
+     * @Template("GajdawLitBundle:Edition:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Edition();
+        $form = $this->createForm(new EditionType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('edition_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to create a new Edition entity.
+     *
+     * @Route("/new", name="edition_new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Edition();
+        $form   = $this->createForm(new EditionType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Finds and displays a Edition entity.
+     *
+     * @Route("/{id}", name="edition_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('GajdawLitBundle:Edition')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Edition entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Edition entity.
+     *
+     * @Route("/{id}/edit", name="edition_edit")
+     * @Method("GET")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('GajdawLitBundle:Edition')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Edition entity.');
+        }
+
+        $editForm = $this->createForm(new EditionType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing Edition entity.
+     *
+     * @Route("/{id}", name="edition_update")
+     * @Method("PUT")
+     * @Template("GajdawLitBundle:Edition:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('GajdawLitBundle:Edition')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Edition entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new EditionType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('edition_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Deletes a Edition entity.
+     *
+     * @Route("/{id}", name="edition_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('GajdawLitBundle:Edition')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Edition entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('edition'));
+    }
+
+    /**
+     * Creates a form to delete a Edition entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Gajdaw/LitBundle/Entity/Edition.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Gajdaw\LitBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Edition
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Edition
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="number", type="string", length=255)
+     */
+    private $number;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set number
+     *
+     * @param string $number
+     * @return Edition
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    
+        return $this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return string 
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+}
